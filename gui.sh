2bf7138803b4f7be086905055884ebc929f176c9
Henry Webel <heweb@dtu.dk>
2026-01-08 15:33:18 +0100

    🚧 more explicit dependencies

--- gui.sh
+++ gui.sh
@@ -4,6 +4,9 @@ pyinstaller gui.py \
     --onedir \
     --collect-all streamlit \
     --collect-all cellpose \
+    --collect-all streamlit_image_coordinates \
+    --collect-all huggingface_hub \
+    --collect-all sam2 \
     --add-data *.py:. \
     --add-data help_texts:help_texts \
     --add-data helpers:helpers \
